cec7729a9d5b14c5dfa7ab5adb085007be2bb84a
Pavlo Pashchevskyi <p.paschevkiy@kitgroup.ua>
2020-09-18 10:49:44 +0300

    Field type: radiobutton or checkbox

--- src/Services/TestService.php
+++ src/Services/TestService.php
@@ -6,6 +6,7 @@ namespace App\Services;
 use App\Repository\TestRepository;
 use App\Entity\Test;
 use Exception;
+use App\Entity\Question;
 
 /**
  * Description of TestService
@@ -33,12 +34,13 @@ class TestService
         
         $commonTestData = $this->testRepository->getTestInfo($test);
         $commonTestData['questions'] = [];
-        /** @var \App\Entity\Question $question */
+        /** @var Question $question */
         foreach ($test->getQuestions() as $i => $question) {
+            $rvq = $this->getRightVariantsQuantity($question);
             $commonTestData['questions'][$i] = [
                 'question_id' => $question->getId(),
-                'type' => $question->getType(),
                 'text' => $question->getText(),
+                'field_type' => ($rvq > 1) ? 1 : 0,
                 'variants' => [],
             ];
             /** @var \App\Entity\Variant $variant */
@@ -50,5 +52,20 @@ class TestService
         shuffle($commonTestData['questions']);
         
         return $commonTestData;
+    }
+    
+    /**
+     * 
+     * @param Question $question
+     * @return int
+     */
+    private function getRightVariantsQuantity(Question $question): int
+    {
+        $rvq = 0;
+        foreach ($question->getVariants() as $variant) {
+            $rvq += ($variant->getValue() > 0) ? 1 : 0;
+        }
+
+        return $rvq;
     }
 }
